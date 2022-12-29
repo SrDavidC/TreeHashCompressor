926d5ae82e3ab0f5c627aa01e51bba404b3bb030
Sagot <jorge.diazsagot@ucr.ac.cr>
2022-12-29 09:50:58 -0600

    Interface miniFix

--- include/Interface.hpp
+++ include/Interface.hpp
@@ -4,13 +4,28 @@
 #include <iostream>
 #include <string>
 
-template <typename T>
+/**
+ * @brief Interface class
+ * 
+ */
 class Interface { 
   private:
   public:
+    /**
+     * @brief Construct a new Interface object
+     * 
+     */
     Interface(){}
+    /**
+     * @brief Destroy the Interface object
+     * 
+     */
     ~Interface(){}
 
+    /**
+     * @brief Print the main menu
+     * 
+     */
     void printMainMenu() {
       std::cout << "Welcome to the main menu!" << std::endl;
       std::cout << "1. Compress" << std::endl;
@@ -18,6 +33,10 @@ class Interface {
       std::cout << "3. Exit" << std::endl;  
     }
 
+    /**
+     * @brief Print the compress menu
+     * 
+     */
     void printCompressMenu() {
       std::cout << "Welcome to the compress menu!" << std::endl;
       std::cout << "1. Compress a file" << std::endl;
@@ -25,6 +44,10 @@ class Interface {
       std::cout << "3. Back" << std::endl;
     }
 
+    /**
+     * @brief Print the decompress menu
+     * 
+     */
     void printDecompressMenu() {
       std::cout << "Welcome to the decompress menu!" << std::endl;
       std::cout << "1. Decompress a file" << std::endl;
@@ -32,35 +55,44 @@ class Interface {
       std::cout << "3. Back" << std::endl;
     }
 
-    T standartInput( char type, short range ) {
-      T input;
+    /**
+     * @brief Get the standart input int object
+     * 
+     * @param range to set the range of the input menu
+     * @return int 
+     */
+    int standartInputInt( short range ) {
+      int input;
       std::string inputStr;
       std::cout << "Insert your input: " << std::endl;
       std::cin >> inputStr;
-      switch (type)
-      {
-      case 'i':
-        while ( std::cin.fail() || !(inputStr > 0 && inputStr <= range) ) {
-          std::cin.clear();
-          std::cin.ignore(256,'\n');
-          std::cout << "Invalid input, try again: " << std::endl;
-          std::cin >> inputStr;
-        }
-        input = stoi(inputStr);
-        break;
-      case 's':
-        while (std::cin.fail()) {
-          std::cin.clear();
-          std::cin.ignore(256,'\n');
-          std::cout << "Invalid input, try again: " << std::endl;
-          std::cin >> inputStr;
-        }
-        input = inputStr;
-        break;
-      default:
-        break;
+      while ( std::cin.fail() || !( stoi(inputStr) > 0 && stoi(inputStr) <= range) ) {
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        std::cout << "Invalid input, try again: " << std::endl;
+        std::cin >> inputStr;
       }
+      input = stoi(inputStr);
+      
       return input;
+    }
+
+    /**
+     * @brief Get the standart input string object
+     * 
+     * @return std::string 
+     */
+    std::string standartInputString() {
+      std::string inputStr;
+      std::cout << "Insert your input: " << std::endl;
+      std::cin >> inputStr;
+      while ( std::cin.fail() ) {
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+        std::cout << "Invalid input, try again: " << std::endl;
+        std::cin >> inputStr;
+      }
+      return inputStr;
     }
 
 };
